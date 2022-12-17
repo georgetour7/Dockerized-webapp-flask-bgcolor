de5d15acb7d5c15148d57dba9fe21aee94aeffc0
georgetour7 <georgetour7@yahoo.com>
2022-12-17 19:02:34 +0200

    Update app.py

--- webapp-flask-bgcolor/app.py
+++ webapp-flask-bgcolor/app.py
@@ -7,20 +7,20 @@ import argparse
 app = Flask(__name__)
 
 color_codes = {
-    "red": "#ff0000",
-    "green": "#00ff00",
-    "blue": "#0000ff",
-    "olive": "#808000",
-    "purple": "#800080",
-    "navy": "#000080"
+    "#ff0000",
+    "#00ff00",
+    "#0000ff",
+    "#808000",
+    "#800080",
+    "#000080"
 }
 
 SUPPORTED_COLORS = ",".join(color_codes.keys())
 
 # Get color from Environment variable
-COLOR_FROM_ENV = os.environ.get('APP_COLOR')
+COLOR_FROM_ENV = os.environ.get('APP_COLORHEXCODE')
 # Generate a random color
-COLOR = random.choice(["red", "green", "blue", "olive", "purple", "navy"])
+COLOR = random.choice(["#ff0000", "#00ff00", "#0000ff", "#808000", "#800080", "#000080"])
 # Get dynamic title from Environment variable
 TITLE_FROM_ENV = os.environ.get('APP_TITLE')
 # Set default title
@@ -35,13 +35,13 @@ def main():
 if __name__ == "__main__":
     print("This is a simple flask webapp that displays a colored background and a greeting message. \n"
           "The color can be specified in two different ways: \n"
-          "    1. As a command line argument with --color as the argument. Accepts one of the following \n"
-          "       colors according the list below. \n"
-          "    2. As an Environment variable APP_COLOR. Accepts one of the following colors according \n"
+          "    1. As a command line argument with --colorhexcode as the argument. Accepts one of the following \n"
+          "       color codes according the list below. \n"
+          "    2. As an Environment variable APP_COLORHEXCODE. Accepts one of the following color codes according \n"
           "       the list below.\n"
           "In any other case, a random color is picked from the list below.\n"
           "\n"
-          "Note 1: Accepted colors [" + SUPPORTED_COLORS + "] \n"
+          "Note 1: Accepted color codes [" + SUPPORTED_COLORS + "] \n"
           "Note 2: Command line argument precedes over environment variable.\n"
           "\n"
           "")
@@ -49,7 +49,7 @@ if __name__ == "__main__":
 
     # Check for Command Line Parameters for color
     parser = argparse.ArgumentParser()
-    parser.add_argument('--color', required=False)
+    parser.add_argument('--colorhexcode', required=False)
     # Check for Command Line Parameters for title
     parser.add_argument('--title', required=False)
     args = parser.parse_args()
