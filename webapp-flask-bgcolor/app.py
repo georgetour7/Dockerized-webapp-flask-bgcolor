5b7dc256438ee341c9f65ab572657c9fd6c007b0
georgetour7 <georgetour7@yahoo.com>
2022-12-18 16:27:43 +0200

    Update app.py

--- webapp-flask-bgcolor/app.py
+++ webapp-flask-bgcolor/app.py
@@ -6,14 +6,14 @@ import argparse
 
 app = Flask(__name__)
 
-color_codes = {
+color_codes = [
     '#ff0000',
     '#00ff00',
     '#0000ff',
     '#808000',
     '#800080',
     '#000080'
-}
+]
 
 SUPPORTED_COLORS = ",".join(color_codes)
 
